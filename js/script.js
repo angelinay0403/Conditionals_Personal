c3d1f5eb2f90b39796ac11fb20756e931a14ddd3
angelinay0403 <angelinay0403@gmail.com>
2013-11-13 23:46:22 -0500

    change the order.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-// JavaScript Document
+//Angelina Castro, 11/13/2013, Conditionals.
+
+
+// Conditional Personal.
+
+// problem: If i eat less than 1,000 calories per day i will lose 3 pounds a week, but if i do excersise i will lose more.
+
+var calories = 900
+// if the calories i eat is more than
+if(calories > 1000){
+	console.log("I will not lose the set goal!")
+}else{
+	//if i eat more calories than
+	console.log("I will lose 3 pounds per week!")
+}
+	
